ac4b6d88cfffd0a03f39301de188d719ad8505e2
ninadfaterpekar <ninad.faterpekar@gmail.com>
2021-10-04 20:05:13 -0400

    Initial Develop commit

---
+++ pages/blog.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Blog() {
+  return (
+    <div>
+      <Head>
+        <title> Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Blog</h1>
+      </main>
+    </div>
+  );
+}
