70c6b37d2e2e4092e5f2fef642d88364e6bd16bb
ninadfaterpekar <ninad.faterpekar@gmail.com>
2021-10-10 18:27:34 -0400

    Contentful connected and items loading

--- pages/blog.js
+++ pages/blog.js
@@ -1,8 +1,25 @@
 import Head from "next/head";
-import { blogPosts } from "../lib/data";
+// import { blogPosts } from "../lib/data";
 import Link from "next/link";
 
-export default function Blog() {
+let client = require("contentful").createClient({
+  space: process.env.NEXT_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.NEXT_CONTENTFUL_ACCESS_TOKEN,
+});
+
+export async function getStaticProps() {
+  let data = await client.getEntries({
+    content_type: "blogPost",
+  });
+  return {
+    props: {
+      blogPosts: data.items,
+    },
+  };
+}
+
+export default function Blog({ blogPosts }) {
+  console.log(blogPosts);
   return (
     <div>
       <Head>
@@ -15,9 +32,9 @@ export default function Blog() {
       </main>
       <div>
         {blogPosts.map((item) => (
-          <div key={item.slug}>
-            <Link as={"/blog/" + item.slug} href="/blog/[slug]">
-              <a>{item.title}</a>
+          <div key={item.sys.id}>
+            <Link as={"/blog/" + item.fields.slug} href="/blog/[slug]">
+              <a>{item.fields.title}</a>
             </Link>
           </div>
         ))}
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,35 +1,62 @@
+import { data } from "autoprefixer";
 import Head from "next/head";
-import { blogPosts } from "../../lib/data";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import Image from "next/image";
 
-export default function blogPage({ title, date, content }) {
+let client = require("contentful").createClient({
+  space: process.env.NEXT_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.NEXT_CONTENTFUL_ACCESS_TOKEN,
+});
+
+export default function blogPage({ blogPosts }) {
+  console.log(blogPosts);
+
+  const blogImage = "https:" + blogPosts.fields.heroImage.fields.file.url;
+
   return (
     <div>
       <Head>
-        <title>{title}</title>
+        <title>{blogPosts.fields.title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1>{title}</h1>
-        <h2>{date}</h2>
-        <p>{content}</p>
+        <h1>{blogPosts.fields.title}</h1>
+        {/* <img src={"https:" + blogPosts.fields.heroImage.fields.file.url}></img> */}
+        <Image
+          loader={() => blogImage}
+          width={700}
+          src={blogImage}
+          height={500}
+        />
+        <p>{blogPosts.fields.body}</p>
+        <p>{blogPosts.fields.description}</p>
+        <p>{blogPosts.fields.heroImage.fields.title}</p>
+        <p>{blogPosts.fields.heroImage.fields.file.url}</p>
       </main>
     </div>
   );
 }
 
-export async function getStaticProps(context) {
-  const { params } = context;
+export async function getStaticProps({ params }) {
+  let data = await client.getEntries({
+    content_type: "blogPost",
+  });
   return {
-    props: blogPosts.find((item) => item.slug === params.slug),
+    props: {
+      blogPosts: data.items[0],
+    },
   };
 }
 
 export async function getStaticPaths() {
+  let data = await client.getEntries({
+    content_type: "blogPost",
+  });
   return {
-    paths: blogPosts.map((item) => ({
+    paths: data.items.map((item) => ({
       params: {
-        slug: item.slug,
+        slug: item.fields.slug,
       },
     })),
     fallback: false,
