36c7c8545b95622a61ef664f6848d1103b94cf76
Dr Disha Shah <dishashah@Drs-MacBook-Pro.local>
2022-01-14 01:09:32 -0500

    Test commit from new mac

--- components/rightImgSection.js
+++ components/rightImgSection.js
@@ -34,7 +34,7 @@ export default function RightImgSection() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
+            aliquip.
           </p>
         </Box>
         <Box
