486adbb19a30dfee4bf6f85e750db65abe29a79b
Dr Disha Shah <dishashah@Drs-MacBook-Pro.local>
2022-02-05 20:31:10 -0500

    test prod

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Layout from "../components/layout";
 
 let client = require("contentful").createClient({
   space: process.env.NEXT_CONTENTFUL_SPACE_ID,
-  accessToken: process.env.NEXT_CONTENTFUL_DELIVERY_ACCESS_TOKEN,
+  accessToken: process.env.NEXT_CONTENTFUL_ACCESS_TOKEN,
 });
 
 const getImgUrl = (imageObject) => {
