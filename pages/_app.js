9fa745b105247c5b0390ca68a1b2b14f017d2e8b
ninadfaterpekar <ninad.faterpekar@gmail.com>
2022-07-26 13:37:22 -0400

    Access Token

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import createEmotionCache from "../src/createEmotionCache";
 
 let client = require("contentful").createClient({
   space: process.env.NEXT_CONTENTFUL_SPACE_ID,
-  accessToken: process.env.NEXT_CONTENTFUL_DELIVERY_ACCESS_TOKEN,
+  accessToken: process.env.NEXT_CONTENTFUL_ACCESS_TOKEN,
 });
 
 export async function getStaticProps() {
